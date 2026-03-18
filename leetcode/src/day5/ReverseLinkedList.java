8ca76296935470a793a3f5c47825b16105b51989
Uday531 <adepuuday51@gmail.com>
2026-03-18 22:42:42 +0530

    day5 of leetcode

---
+++ leetcode/src/day5/ReverseLinkedList.java
@@ -0,0 +1,59 @@
+package day5;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    // Constructor
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedList {
+
+    // Function to reverse the linked list
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next; // store next
+            curr.next = prev;          // reverse link
+            prev = curr;               // move prev
+            curr = next;               // move curr
+        }
+
+        return prev; // new head
+    }
+
+    // Utility function to print list
+    public static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+
+    // Main method to test
+    public static void main(String[] args) {
+
+        // Creating linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.print("Original List: ");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.print("Reversed List: ");
+        printList(head);
+    }
+}
