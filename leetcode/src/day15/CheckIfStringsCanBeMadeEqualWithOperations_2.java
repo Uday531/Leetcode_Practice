d6b5f0cefb80518cdab4fbbb4006710e0c689962
Uday531 <adepuuday51@gmail.com>
2026-03-30 23:33:57 +0530

    leetcode daily challenges

---
+++ leetcode/src/day15/CheckIfStringsCanBeMadeEqualWithOperations_2.java
@@ -0,0 +1,29 @@
+package day15;
+
+public class CheckIfStringsCanBeMadeEqualWithOperations_2 {
+    static void main() {
+        String s1="abcdba";
+        String s2="cabdab";
+        System.out.println(checkIfString_2(s1,s2));
+    }
+    public static boolean checkIfString_2(String s1,String s2){
+        int[] even=new int[26];
+        int[] odd=new int[26];
+        for(int i=0;i<s1.length();i++){
+            if(i%2==0){
+                even[s1.charAt(i)-'a']++;
+                even[s2.charAt(i)-'a']--;
+            }
+            else {
+                odd[s1.charAt(i)-'a']++;
+                odd[s2.charAt(i)-'a']--;
+            }
+        }
+        for(int i=0;i<26;i++){
+            if(even[i]!=0 || odd[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
