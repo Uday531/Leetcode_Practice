92852f9daad1e348295474174d5cdf68971acffb
Uday531 <adepuuday51@gmail.com>
2026-03-30 12:09:35 +0530

    full code

--- leetcode/src/day13/BinaryTreeInorderTraversal_94.java
+++ leetcode/src/day13/BinaryTreeInorderTraversal_94.java
@@ -5,25 +5,25 @@ import java.util.List;
 
 public class BinaryTreeInorderTraversal_94 {
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(1,
-                new TreeNode(2,
-                        new TreeNode(4, null, null),
-                        new TreeNode(5, null, null)
+        TreeNode12 root = new TreeNode12(1,
+                new TreeNode12(2,
+                        new TreeNode12(4, null, null),
+                        new TreeNode12(5, null, null)
                 ),
-                new TreeNode(3, null, null)
+                new TreeNode12(3, null, null)
         );
 
         List<Integer> result = treetraversal(root);
         System.out.println(result); // Expected: [4, 2, 5, 1, 3]
     }
 
-    public static List<Integer> treetraversal(TreeNode root) {
+    public static List<Integer> treetraversal(TreeNode12 root) {
         List<Integer> l = new ArrayList<>();
         hell(root, l);
         return l;
     }
 
-    public static void hell(TreeNode node, List<Integer> res) {
+    public static void hell(TreeNode12 node, List<Integer> res) {
         if (node == null) {
             return;
         }
@@ -32,12 +32,12 @@ public class BinaryTreeInorderTraversal_94 {
         hell(node.right, res);  // Right
     }
 
-    public static class TreeNode {
+    public static class TreeNode12 {
         int data;
-        TreeNode left;
-        TreeNode right;
+        TreeNode12 left;
+        TreeNode12 right;
 
-        TreeNode(int data, TreeNode left, TreeNode right) {
+        TreeNode12(int data, TreeNode12 left, TreeNode12 right) {
             this.data = data;
             this.left = left;
             this.right = right;
--- leetcode/src/day14/SameTree_100.java
+++ leetcode/src/day14/SameTree_100.java
@@ -2,7 +2,15 @@ package day14;
 
 public class SameTree_100 {
     static void main() {
-        System.out.println("hi");
+        MaximumDepthOfBinaryTrees.TreeNode12 root1 = new MaximumDepthOfBinaryTrees.TreeNode12(1,
+                new MaximumDepthOfBinaryTrees.TreeNode12(2,null,null),
+                new MaximumDepthOfBinaryTrees.TreeNode12(3,null,null)
+        );
+        MaximumDepthOfBinaryTrees.TreeNode12 root2 = new MaximumDepthOfBinaryTrees.TreeNode12(1,
+                new MaximumDepthOfBinaryTrees.TreeNode12(2,null,null),
+                new MaximumDepthOfBinaryTrees.TreeNode12(4,null,null)
+        );
+        System.out.println(sameTree(root1,root2));
     }
     public static boolean sameTree(MaximumDepthOfBinaryTrees.TreeNode12 root1, MaximumDepthOfBinaryTrees.TreeNode12 root2){
         if(root1==null && root2==null){
@@ -11,7 +19,7 @@ public class SameTree_100 {
         if(root1==null || root2==null || root1.data!=root2.data){
             return false;
         }
-        return sameTree(root1.left,root1.right) && sameTree(root2.left,root2.right);
+        return sameTree(root1.left,root2.left) && sameTree(root1.right,root2.right);
     }
 }
 
