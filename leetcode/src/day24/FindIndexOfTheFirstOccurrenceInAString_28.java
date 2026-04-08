d32b573e5feb541c2f60bd8af26a48e26025b8fe
Uday531 <adepuuday51@gmail.com>
2026-04-08 23:51:15 +0530

    day24 of leetcode

---
+++ leetcode/src/day24/FindIndexOfTheFirstOccurrenceInAString_28.java
@@ -0,0 +1,19 @@
+package day24;
+
+public class FindIndexOfTheFirstOccurrenceInAString_28 {
+    static void main() {
+        String a="sadbutbad";
+        String b="bad";
+        System.out.println(firstOccurrence(a,b));
+    }
+    public static int firstOccurrence(String a,String b){
+        for(int i=0;i<a.length()-b.length()+1;i++){
+            if(a.charAt(i)==b.charAt(0)){
+                if(a.substring(i,b.length()+i).equals(b)){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
