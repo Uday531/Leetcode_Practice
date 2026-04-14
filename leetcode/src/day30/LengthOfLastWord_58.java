5ed6e7abb4cb27cee533236d05de61a84596c1f2
Uday531 <adepuuday51@gmail.com>
2026-04-14 23:53:09 +0530

    day30 of leetcode, finally 1 month

---
+++ leetcode/src/day30/LengthOfLastWord_58.java
@@ -0,0 +1,20 @@
+package day30;
+
+public class LengthOfLastWord_58 {
+    static void main() {
+        String s="Hello World";
+        System.out.println(lenOfWord(s));
+    }
+    public static int lenOfWord(String s){
+       int count=0;
+       int r=s.length()-1;
+       for(int i=r;i>=0;i--){
+           if (s.charAt(i)!=' '){
+               count++;
+           }else {
+               break;
+           }
+       }
+       return count;
+    }
+}
