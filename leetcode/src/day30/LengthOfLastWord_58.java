5cd8446ea16c7ccd333633a3cd98e37c28ae968d
Uday531 <adepuuday51@gmail.com>
2026-04-14 23:57:12 +0530

    day30 of leetcode, finally 1 month

--- leetcode/src/day30/LengthOfLastWord_58.java
+++ leetcode/src/day30/LengthOfLastWord_58.java
@@ -6,10 +6,11 @@ public class LengthOfLastWord_58 {
         System.out.println(lenOfWord(s));
     }
     public static int lenOfWord(String s){
+        String tst=s.trim();
        int count=0;
-       int r=s.length()-1;
+       int r=tst.length()-1;
        for(int i=r;i>=0;i--){
-           if (s.charAt(i)!=' '){
+           if (tst.charAt(i)!=' '){
                count++;
            }else {
                break;
