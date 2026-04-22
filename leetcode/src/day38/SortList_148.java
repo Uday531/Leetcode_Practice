5b646e76fbf0ca185bbed8f4792582a70cb58606
Uday531 <adepuuday51@gmail.com>
2026-04-23 00:04:24 +0530

    day38 of leetcodeee

--- leetcode/src/day38/SortList_148.java
+++ leetcode/src/day38/SortList_148.java
@@ -2,6 +2,66 @@ package day38;
 
 public class SortList_148 {
     static void main() {
-
+        ListNode l=new ListNode(1);
+        l.next=new ListNode(2);
+        l.next.next=new ListNode(4);
+        l.next.next.next=new ListNode(3);
+        ListNode res=sortList(l);
+        printList(res);
     }
+    public static ListNode sortList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode mid = getmid(head);
+        ListNode rightHead = mid.next;
+        mid.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(rightHead);
+        return merge(left, right);
+    }
+    private static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+    private static ListNode getmid(ListNode head){
+        ListNode s=head,f=head.next;
+        while(f!=null && f.next!=null){
+            s=s.next;
+            f=f.next.next;
+        }
+        return s;
+    }
+    private static ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        // attach remaining
+        if (l1 != null) tail.next = l1;
+        if (l2 != null) tail.next = l2;
+
+        return dummy.next;
+    }
+}
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int val){
+        this.val=val;
+    }
+
 }
