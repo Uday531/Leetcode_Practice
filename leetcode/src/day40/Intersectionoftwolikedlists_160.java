751c30fd0389e6e26e986f0634ab237653327db3
Uday531 <adepuuday51@gmail.com>
2026-04-25 00:01:31 +0530

    day40 of leetcodeee

--- leetcode/src/day40/Intersectionoftwolikedlists_160.java
+++ leetcode/src/day40/Intersectionoftwolikedlists_160.java
@@ -1,7 +1,36 @@
 package day40;
+import java.util.*;
 
 public class Intersectionoftwolikedlists_160 {
     static void main() {
+        ListNode l=new ListNode(1);
+        l.next=new ListNode(2);
+        l.next.next=new ListNode(3);
+        ListNode rs=getIntersectionNode(l,l);
+        printList(rs);
+    }
 
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a=headA;
+        ListNode b=headB;
+        while(a!=b){
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+        return a;
+    }
+    private static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+}
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int val){
+        this.val=val;
     }
 }
