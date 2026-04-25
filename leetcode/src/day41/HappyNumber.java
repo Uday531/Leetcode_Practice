8e8dd295812108f8eca93e6839234aeef4f9b3b3
Uday531 <adepuuday51@gmail.com>
2026-04-26 00:00:02 +0530

    day41 of leetcodeee

--- leetcode/src/day41/HappyNumber.java
+++ leetcode/src/day41/HappyNumber.java
@@ -1,7 +1,26 @@
 package day41;
+import java.util.*;
 
 public class HappyNumber {
     static void main() {
-
+        int n=12;
+        System.out.println(isHappy(n));
+    }
+    public static boolean isHappy(int n) {
+        HashSet<Integer> h=new HashSet<>();
+        while(n!=1 && !h.contains(n)){
+            h.add(n);
+            n=getSum(n);
+        }
+        return n==1;
+    }
+    private static int getSum(int n){
+        int sum=0;
+        while(n>0){
+            int d=n%10;
+            sum+=d*d;
+            n=n/10;
+        }
+        return sum;
     }
 }
