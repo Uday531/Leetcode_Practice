507cb797f96a926d154958bc41ac10b8c66bfd25
Uday531 <adepuuday51@gmail.com>
2026-04-27 23:18:04 +0530

    day43 of leetcodeee

---
+++ leetcode/src/day43/ValidSudoku_36.java
@@ -0,0 +1,45 @@
+package day43;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class ValidSudoku_36 {
+    static void main() {
+        String[][] a={{"5","3",".",".","7",".",".",".","."}
+,{"6",".",".","1","9","5",".",".","."}
+,{".","9","8",".",".",".",".","6","."}
+,{"8",".",".",".","6",".",".",".","3"}
+,{"4",".",".","8",".","3",".",".","1"}
+,{"7",".",".",".","2",".",".",".","6"}
+,{".","6",".",".",".",".","2","8","."}
+,{".",".",".","4","1","9",".",".","5"}
+,{".",".",".",".","8",".",".","7","9"}};
+        System.out.println(isValidSudoku(a));
+    }
+    public static boolean isValidSudoku(String[][] s){
+        HashMap<Integer,HashSet<String>> row=new HashMap<>();
+        HashMap<Integer,HashSet<String>> col=new HashMap<>();
+        HashMap<Integer,HashSet<String>> box=new HashMap<>();
+        for (int i=0;i<s.length;i++){
+            row.put(i,new HashSet<>());
+            col.put(i,new HashSet<>());
+            box.put(i,new HashSet<>());
+        }
+        for (int i=0;i<9;i++){
+            for (int j=0;j<9;j++){
+                String num=s[i][j];
+                if (num.equals(".")){
+                    continue;
+                }
+                int boxindex=(i/3)*3+(j/3);
+                if (row.get(i).contains(num) || col.get(j).contains(num) || box.get(boxindex).contains(num)){
+                    return false;
+                }
+                row.get(i).add(num);
+                col.get(j).add(num);
+                box.get(boxindex).add(num);
+            }
+        }
+        return true;
+    }
+}
