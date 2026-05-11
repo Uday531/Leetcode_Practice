66f336d87b929669ec26e05ec36310133328bb07
Uday531 <adepuuday51@gmail.com>
2026-05-11 23:26:14 +0530

    day54 of leetcodeee

---
+++ leetcode/src/day54/SeperateDigitsInAnArray_2553.java
@@ -0,0 +1,26 @@
+package day54;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SeperateDigitsInAnArray_2553 {
+    static void main() {
+        int[] a={12,34,56};
+        System.out.println(Arrays.toString(seperateDigits(a)));
+    }
+    public static int[] seperateDigits(int[] a){
+        List<Integer> l=new ArrayList<>();
+        for (int n:a){
+            String s=String.valueOf(n);
+            for (char c:s.toCharArray()){
+                l.add(c-'0');
+            }
+        }
+        int[] res=new int[l.size()];
+        for (int i=0;i<l.size();i++){
+            res[i]=l.get(i);
+        }
+        return res;
+    }
+}
