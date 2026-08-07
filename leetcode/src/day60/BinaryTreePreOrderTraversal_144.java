f98d7ca02ff2077946e83387145f65bff57f9924
Uday531 <adepuuday51@gmail.com>
2026-08-07 10:50:28 +0530

    day57 of leetcodeee

--- leetcode/src/day60/BinaryTreePreOrderTraversal_144.java
+++ leetcode/src/day60/BinaryTreePreOrderTraversal_144.java
@@ -1,14 +1,22 @@
 package day60;
 
-import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
 public class BinaryTreePreOrderTraversal_144 {
     static void main() {
+        TreeNode n=new TreeNode(1);
+        n.left=new TreeNode(2);
+        n.right=new TreeNode(3);
+        n.left.left=new TreeNode(4);
+        n.left.right=new TreeNode(5);
+        n.right.left=new TreeNode(6);
+        n.right.right=new TreeNode(7);
+        System.out.println(preorderTraversal(n));
 
     }
-    public List<Integer> preorderTraversal(TreeNode root) {
+    public static List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
 
         if (root == null) {
@@ -20,7 +28,7 @@ public class BinaryTreePreOrderTraversal_144 {
 
         while (!stack.isEmpty()) {
             TreeNode curr = stack.pop();
-            ans.add(curr.val);
+            ans.add(curr.data);
 
             if (curr.right != null) {
                 stack.push(curr.right);
@@ -33,4 +41,13 @@ public class BinaryTreePreOrderTraversal_144 {
 
         return ans;
     }
+    static class TreeNode{
+        int data;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int data){
+            this.data=data;
+        }
+    }
+
 }
