ce086321d98558b3d74d546dc6c330c206a04b42
Uday531 <adepuuday51@gmail.com>
2026-08-16 00:49:48 +0530

    day67 of leetcodeee

---
+++ leetcode/src/day67/Valid_Anagram.java
@@ -0,0 +1,26 @@
+package day67;
+
+
+public class Valid_Anagram {
+    static void main() {
+        String s1="anagram";
+        String s2="nagarqm";
+        System.out.println(isValid(s1,s2));
+    }
+    public static boolean isValid(String s1, String s2){
+        int[] freq=new int[26];
+        if (s1.length()!=s2.length()){
+            return false;
+        }
+        for (int i=0;i<s1.length();i++){
+            freq[s1.charAt(i)-'a']++;
+            freq[s2.charAt(i)-'a']--;
+        }
+        for (int c:freq){
+            if (c!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
