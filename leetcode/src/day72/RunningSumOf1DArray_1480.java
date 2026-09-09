ba083ee59187aa1891fd90c3cbdb58e6408fcc69
Uday531 <adepuuday51@gmail.com>
2026-09-09 11:39:40 +0530

    day72 of leetcodeee 2

---
+++ leetcode/src/day72/RunningSumOf1DArray_1480.java
@@ -0,0 +1,19 @@
+package day72;
+
+import java.util.Arrays;
+
+public class RunningSumOf1DArray_1480 {
+    static void main() {
+        int[] nums={1,2,3,4};
+        System.out.println(Arrays.toString(runningSum(nums)));
+    }
+    public static int[] runningSum(int[] nums) {
+        int[] runningSum=new int[nums.length];
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+            runningSum[i]=sum;
+        }
+        return runningSum;
+    }
+}
